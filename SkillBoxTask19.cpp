a6c45769b09ceda46aa575dad0fa1a32f177bc22
Perl0ovkA <104366771+Perl0ovkA@users.noreply.github.com>
2024-07-23 17:15:01 +0300

    Добавьте файлы проекта.

---
+++ SkillBoxTask19.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class Animal
+{
+public:
+	virtual void AnimalSound() = 0;
+};
+
+class Dog : public Animal
+{
+public:
+	void AnimalSound() override
+	{
+		cout << "Woof!\n";
+	}
+};
+
+class Cat : public Animal
+{
+public:
+	void AnimalSound() override
+	{
+		cout << "Meow!\n";
+	}
+};
+
+class Cow : public Animal
+{
+public:
+	void AnimalSound() override
+	{
+		cout << "Mooo!\n";
+	}
+};
+
+int main()
+{
+	Animal* animals[3];
+	animals[0] = new Dog();
+	animals[1] = new Cat();
+	animals[2] = new Cow();
+
+	for (Animal* a : animals)
+		a->AnimalSound();
+}
